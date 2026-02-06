9af534934b2e06ff06ca7b2cf61ef8e0f793e256
Jimmyy85 <jimmmy.caillon@gmail.com>
2026-02-07 00:27:54 +0100

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // !! ATTENTION !!
-    // Cela permet de déployer même s'il y a des erreurs TypeScript.
+    // Ignore les erreurs de type lors du build pour éviter le blocage
     ignoreBuildErrors: true,
   },
+  // Dans les versions récentes, on gère ESLint comme ceci :
   eslint: {
-    // On ignore aussi les erreurs de style (ESLint) pour le déploiement.
     ignoreDuringBuilds: true,
   },
 };
