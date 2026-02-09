a20907cc5077d60bd0d2e769822763fcf4bfb274
Jimmyy85 <jimmmy.caillon@gmail.com>
2026-02-09 22:12:15 +0100

    Add files via upload

---
+++ app/actions.ts
@@ -0,0 +1,19 @@
+﻿"use server";
+
+export async function getDeezerData(endpoint: string) {
+  try {
+    // Le serveur Vercel appelle Deezer directement (Pas de CORS ici)
+    const response = await fetch(`https://api.deezer.com${endpoint}`, {
+      cache: 'no-store' // On force la mise à jour à chaque fois
+    });
+    
+    if (!response.ok) {
+      return { data: [] };
+    }
+    
+    return await response.json();
+  } catch (error) {
+    console.error("Erreur Serveur Deezer:", error);
+    return { data: [] };
+  }
+}
