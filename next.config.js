fa510ea51d484efdc5dc46801c9e3a0363d7f647
Jimmyy85 <jimmmy.caillon@gmail.com>
2026-02-09 21:29:53 +0100

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,13 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    // Ignore les erreurs de type lors du build pour éviter le blocage
-    ignoreBuildErrors: true,
-  },
-  // Dans les versions récentes, on gère ESLint comme ceci :
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+    // Cette partie autorise les images externes
+    images: {
+        remotePatterns: [
+            { protocol: 'https', hostname: 'e-cdns-images.dzcdn.net' },
+            { protocol: 'https', hostname: 'api.deezer.com' },
+            { protocol: 'https', hostname: 'api.dicebear.com' },
+        ],
+        unoptimized: true,
+    },
+    // Cette partie crée le "Tunnel" pour éviter le blocage CORS
+    async rewrites() {
+        return [
+            {
+                source: '/api/deezer/:path*',
+                destination: 'https://api.deezer.com/:path*',
+            },
+        ];
+    },
 };
 
 export default nextConfig;
